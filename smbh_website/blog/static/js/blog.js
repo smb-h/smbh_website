9a7785d9dfecd8dedaaca083116b3d02c4843a49
smbh <smb_h@yahoo.com>
2018-08-15 22:10:32 +0430

    finish comment section

--- smbh_website/blog/static/js/blog.js
+++ smbh_website/blog/static/js/blog.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
 
 
+    // Comments
+    // $('.comment-reply').css('display', 'none');
+    $('.comment-reply-btn').click(function(event) {
+        event.preventDefault();
+        $(this).parent().next('.comment-reply').fadeToggle();
+        
+    });
+
+
 });
